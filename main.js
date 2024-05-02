65c32bfd7c20208e72cb4fceda6afba08823c33c
Lidiane Yumi <127134394+proflidianemd@users.noreply.github.com>
2024-05-02 09:05:37 -0300

    1

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-let nome_usuário prompt("Qual o seu nome?");
+
+let nomeUsuário = prompt("Qual o seu nome?");
 let elemento = document.querySelector("#nome-usuário");
 elemento.textContent = nomeUsuário;
